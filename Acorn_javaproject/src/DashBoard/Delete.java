a00a7a4463236e7ca7de004245485fde6c6b4ac9
move-gyu <tpdmsehdrb@naver.com>
2025-09-11 09:48:40 +0900

    20250911_gyu 삭제기능 추가

---
+++ Acorn_javaproject/src/DashBoard/Delete.java
@@ -0,0 +1,28 @@
+package DashBoard;
+
+import java.util.List;
+
+public class Delete {
+    public void delete(List<Board> boardList, int id, String fileName) {
+        
+        boolean removed = boardList.removeIf(board -> board.getId() == id);   //id값 비교 후 삭제
+
+        if (removed) {
+            System.out.println("게시글 " + id + "번이 삭제되었습니다.");
+            // 삭제 후 CSV 파일 저장
+            FileManager.saveData(fileName, boardList);
+        } else {
+            System.out.println("해당 번호의 게시글이 존재하지 않습니다.");
+        }
+    }
+}
+
+
+//Delete delete = new Delete();
+//int deleteId = 3;
+//
+////삭제 실행
+//delete.deletePost(boardList, deleteId, "src/DataBase/data.csv");
+//
+////삭제 후 전체 목록 출력
+//br.postShowAll();
---
+++ Acorn_javaproject/src/DashBoard/ReadFile.java
@@ -0,0 +1,14 @@
+//package DashBoard;
+//
+//import java.util.ArrayList;
+//
+//public class  ReadFile{
+//	
+//		public static void readFile() {
+//	        ArrayList<Board> list = CsvReader.readBoardList(FILE_PATH);
+//	        for(Board b : list) {
+//	            System.out.println(b.getId() + ". " + b.getTitle() + " | " + b.getContent() + " | " + b.getRegDate());
+//	        
+//	    }
+//	}
+//}
